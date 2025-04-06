990faabdd5b6bdb5d372f951f828775cd011a6fb
maazalirao <abrahamlincoln1027@gmail.com>
2025-04-07 04:53:27 +0500

    clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <main>
         <h1>Hello World</h1>
+        <Button>Click me</Button>
       </main>
       </div>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { useUser } from "@clerk/nextjs";
+
+function Header() {
+    const {user} = useUser();
+  return (
+    <div>
+    {user && (
+        <h1>{user?.firstName}
+        {`'s`} Space</h1>
+    )
+    }
+    </div>
+  )
+
+}
+
+export default Header
